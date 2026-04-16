12393b01267a079e6094b1947bd577686c2b4406
zlatandimitrov <z.dimitrov308@proton.me>
2026-04-16 06:58:08 -0400

    run mmore epochs

--- detector_optimization_v4/common_gpu_auto_run_notebook_batch.sh
+++ detector_optimization_v4/common_gpu_auto_run_notebook_batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p gpu_requeue 	
-#SBATCH --mem=150g        			
-#SBATCH --time=04:00:00 			
+#SBATCH --mem=50g        			
+#SBATCH --time=12:00:00 			
 #SBATCH -c 4            			
 #SBATCH --gres=gpu:1        
 #SBATCH --constraint=a100
@@ -11,4 +11,4 @@ module load python
 
 conda activate multiproc_env
 
-python auto_run_notebook.py SWGOLO7_optimization_tr.ipynb
+python auto_run_notebook.py SWGOLO7_optimization_tr_20k_center_init_angle_energy_adam_lr1_mean_u.ipynb
