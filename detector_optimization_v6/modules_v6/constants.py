5becc5c18195572939aad115961f0afa11b1c3ab
zlatandimitrov <z.dimitrov308@proton.me>
2026-07-17 04:14:31 -0400

    update tau injection script, smoke test batch scirpt

--- detector_optimization_v6/modules_v6/constants.py
+++ detector_optimization_v6/modules_v6/constants.py
@@ -117,7 +117,8 @@ BATCH_SIZE_TRAIN  = 20
 # Energies are filtered to the generator's trained band [10**LOG_E_MIN,
 # 10**LOG_E_MAX] GeV inside the loader.
 USE_TAU_PRIMARIES = True
-TAU_WHOLESKY_PATH = "/n/home05/zdimitrov/tambo/TambOpt/detector_optimization_v6/tau_wholesky.h5"
+# TAU_WHOLESKY_PATH = "/n/home05/zdimitrov/tambo/TambOpt/detector_optimization_v6/tau_wholesky.h5"
+TAU_WHOLESKY_PATH = "/n/home05/zdimitrov/tambo/TambOpt/detector_optimization_v6/decay_locations/tau_wholesky.h5"
 TAU_CORPUS_PATH   = os.path.join(SHOWER_CACHE, "cashed_showers_tau_dual.pt")
 
 # Corpus the Step-1 builder reads. Tau runs use a fixed-name file (the pair count
