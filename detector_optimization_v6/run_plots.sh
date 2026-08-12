044ed5444df9ccfba3f9944b50db660baa3b0f5a
zlatandimitrov <z.dimitrov308@proton.me>
2026-08-12 10:46:52 -0400

    update plots sh

--- detector_optimization_v6/run_plots.sh
+++ detector_optimization_v6/run_plots.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#SBATCH -p test
+#SBATCH -p gpu_test
 #SBATCH --mem=60g
 #SBATCH --time=01:00:00
+#SBATCH --gres=gpu:1
 #SBATCH -c 8
 #SBATCH -J run_plots
 #SBATCH -o slurm_logs/slurm-%j-%x.out
@@ -14,10 +15,10 @@ conda deactivate
 
 conda activate multiproc_env
 
-# python -u plots/02_plot_nn_target_vs_pred.py --dual
+python -u plots/02_plot_nn_target_vs_pred.py --dual
 
 # mp4 by default: GIF caps at 50 fps. Needs imageio-ffmpeg.
-python -u plots/04_plot_trajectory_gif.py
+# python -u plots/04_plot_trajectory_gif.py
 
 # python -u plots/04_plot_trajectory_gif.py --monotonic --seconds 20 \
 #     -o layout_trajectory_monotonic.gif
